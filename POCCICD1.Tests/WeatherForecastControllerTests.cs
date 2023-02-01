2d00f56fdd9c17c42ff31e010e77fe108f12e62e
Bernardo Behring <Bernardo.Behring88@gmail.com>
2023-02-01 22:23:41 +0000

    Initial commit

---
+++ POCCICD1.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using POCCICD1;
+using POCCICD1.Controllers;
+
+[TestFixture]
+public class WeatherForecastControllerTests
+{
+    [Test]
+    public void Get_Returns_WeatherForecasts()
+    {
+        // Arrange
+        var logger = Substitute.For<ILogger<WeatherForecastController>>();
+        var controller = new WeatherForecastController(logger);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.InstanceOf<IEnumerable<WeatherForecast>>());
+    }
+}
